36a8bba9b0209b9f67ae58262941373fc38bafb2
JedwardMook <jedwardmook@yahoo.com>
2024-07-07 23:12:46 -0500

    tests passing

--- lib/Course.py
+++ lib/Course.py
@@ -1,5 +1,5 @@
 class Course:
-  def __init__(self, title, schedule, description):
+  def __init__(self, title= None, schedule= None, description= None):
     self.title = title
     self.schedule = schedule
     self.description = description
